460da9c7358d919bcf4d20d93fea842bd0f07b7f
phamhuudat <phamhuudatk58@gmail.com>
2023-02-01 17:03:24 +0700

    load html with ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,42 @@
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:async';
+import 'dart:io';
+//import 'package:webview_flutter/webview_flutter.dart';
 // Import for Android features.
-import 'package:webview_flutter_android/webview_flutter_android.dart';
+//import 'package:webview_flutter_android/webview_flutter_android.dart';
 // Import for iOS features.
-import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+//import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+final InAppLocalhostServer localhostServer = InAppLocalhostServer(
+    port: 8888);
 // #enddocregion platform_imports
-void main() {
+Future<void> main() async {
+
+  //"setEnvironment('AAA','VI')";
+    WidgetsFlutterBinding.ensureInitialized();
+    localhostServer.start();
+    if (Platform.isAndroid) {
+      await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
+
+      var swAvailable = await AndroidWebViewFeature.isFeatureSupported(
+          AndroidWebViewFeature.SERVICE_WORKER_BASIC_USAGE);
+      var swInterceptAvailable = await AndroidWebViewFeature.isFeatureSupported(
+          AndroidWebViewFeature.SERVICE_WORKER_SHOULD_INTERCEPT_REQUEST);
+
+      if (swAvailable && swInterceptAvailable) {
+        AndroidServiceWorkerController serviceWorkerController =
+        AndroidServiceWorkerController.instance();
+
+        await serviceWorkerController
+            .setServiceWorkerClient(AndroidServiceWorkerClient(
+          shouldInterceptRequest: (request) async {
+            print(request);
+            return null;
+          },
+        ));
+      }
+    }
   runApp(const MyApp());
 }
 
@@ -19,15 +49,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -37,16 +58,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -54,111 +65,51 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late final WebViewController _controller;
-  String script = "setEnvironment(\"AAA\");";
-  //"setEnvironment('AAA','VI')";
-
+  late final InAppWebViewController _controller;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    late final PlatformWebViewControllerCreationParams params;
-    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
-      params = WebKitWebViewControllerCreationParams(
-        allowsInlineMediaPlayback: true,
-        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
-      );
-    } else {
-      params = const PlatformWebViewControllerCreationParams();
-    }
-
-    final WebViewController controller =
-    WebViewController.fromPlatformCreationParams(params);
-// ···
-    if (controller.platform is AndroidWebViewController) {
-      AndroidWebViewController.enableDebugging(true);
-      (controller.platform as AndroidWebViewController)
-          .setMediaPlaybackRequiresUserGesture(false);
-    }
-    controller
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(Colors.yellowAccent)
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progress) {
-            debugPrint('WebView is loading (progress : $progress%)');
-          },
-          onPageStarted: (String url) {
-            debugPrint('Page started loading: $url');
-          },
-          onPageFinished: (String url) {
-            debugPrint('Page finished loading: $url');
-          },
-          onWebResourceError: (WebResourceError error) {
-            debugPrint('''
-Page resource error:
-  code: ${error.errorCode}
-  description: ${error.description}
-  errorType: ${error.errorType}
-  isForMainFrame: ${error.isForMainFrame}
-          ''');
-          },
-          onNavigationRequest: (NavigationRequest request) {
-            if (request.url.startsWith('https://www.youtube.com/')) {
-              debugPrint('blocking navigation to ${request.url}');
-              return NavigationDecision.prevent;
-            }
-            debugPrint('allowing navigation to ${request.url}');
-            return NavigationDecision.navigate;
-          },
-        ),
-      )..addJavaScriptChannel(
-      'Toaster',
-      onMessageReceived: (JavaScriptMessage message) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text(message.message)),
-        );
-      },
-    );
-    _controller= controller;
-    _controller.loadFlutterAsset('assets/charting_library/chart_white.html');
-    _controller.runJavaScript(script);
-    //_controller.runJavaScript('initOnReady();');
-
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: WebViewWidget(
-        controller: _controller,
+      body: InAppWebView(
+        initialUrlRequest: URLRequest(
+            url: Uri.parse("http://localhost:8888/assets/charting_library/chart_white.html")),
+        androidOnPermissionRequest: (controller, origin, resources) async {
+          return PermissionRequestResponse(
+              resources: resources,
+              action: PermissionRequestResponseAction.GRANT);
+        },
+        onWebViewCreated: (controller) {
+          String script = "setEnvironment(\"AAA\", \"VI\");";
+          controller.evaluateJavascript(source: script);
+          _controller = controller;
+        },
+        onLoadStart: (controller, url) {
+          print('loadStart ${url}');
+        },
+        onLoadStop: (controller, url) {
+          String script = "setEnvironment(\"AAA\", \"VI\");";
+          controller.evaluateJavascript(source: script);
+          //_controller = controller;
+          print('loadStop ${url}');
+        },
+        onConsoleMessage: (controller, message) {
+          print('loadStop ${message.message}');
+        },
+
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () async {
-          print('html: ${(await _controller.getTitle())!}');
-          String title = (await _controller.getTitle())!;
-          if(title == "about:blank"){
-            _controller.loadFlutterAsset("assets/charting_library/chart_white.html");
-          }
-          _controller.runJavaScript('initOnReady();');
-          //_controller.runJavaScript(script);
-
-          //_controller.runJavaScript(script);
-          //_controller.runJavaScript('initOnReady();');
-          //_controller.loadFlutterAsset("assets/charting_library/chart_white.html");
-          //_controller.runJavaScript(script);
+        onPressed: () {
+          String script = "setEnvironment(\"AAA\", \"VI\");";
+          _controller.evaluateJavascript(source: script);
         },
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
